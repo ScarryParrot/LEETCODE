b9d0737610229f32bf90a1b00b6448535da036ed
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2024-07-08 01:38:27 +0530

    Time: 278 ms (34.38%) | Memory: 25.7 MB (50.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,13 +1,30 @@
+from typing import List
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n=len(nums)
-        pre=1
-        suff=1
-        dp=[0]*n
-        for i in range(n):
-            dp[i]=pre
-            pre*=nums[i]
-        for i in range(n-1,-1,-1):
-            dp[i] *=suff
-            suff*=nums[i]
-        return dp
+        n = len(nums)
+        prod = 1
+        zero_count = 0
+        
+        # Calculate the product of all non-zero elements and count zeros
+        for num in nums:
+            if num != 0:
+                prod *= num
+            else:
+                zero_count += 1
+        
+        # If more than one zero, all products will be zero
+        if zero_count > 1:
+            return [0] * n
+        
+        arr = []
+        for num in nums:
+            if zero_count == 0:
+                arr.append(prod // num)
+            else:
+                if num == 0:
+                    arr.append(prod)
+                else:
+                    arr.append(0)
+        
+        return arr
