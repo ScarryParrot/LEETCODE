1076d38e093f8092da46fba5f5ce62ab61d343cb
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2024-07-08 13:14:44 +0530

    Time: 1872 ms (5.01%) | Memory: 16.6 MB (27.30%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,30 +1,28 @@
-from typing import List
-
+# Python3
+# Better Solution
+from collections import defaultdict
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
-        results = []
-        length = len(nums)
+        n = len(nums)
+        ans = set()
+        hmap = defaultdict(int)
+        for i in nums:
+            hmap[i] += 1
         
-        for i in range(length - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            for j in range(i + 1, length - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                left, right = j + 1, length - 1
-                while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
-                    if total == target:
-                        results.append([nums[i], nums[j], nums[left], nums[right]])
-                        while left < right and nums[left] == nums[left + 1]:
-                            left += 1
-                        while left < right and nums[right] == nums[right - 1]:
-                            right -= 1
-                        left += 1
-                        right -= 1
-                    elif total < target:
-                        left += 1
-                    else:
-                        right -= 1
-        return results
+        for i in range(n-3):
+            hmap[nums[i]] -= 1
+            for j in range(i+1, n-2):
+                hmap[nums[j]] -= 1
+                for k in range(j+1, n-1):
+                    hmap[nums[k]] -= 1
+                    rem = target-(nums[i] + nums[j] + nums[k])
+                    if rem in hmap and hmap[rem] > 0:
+                        ans.add(tuple(sorted((nums[i], nums[j], nums[k], rem))))
+                    hmap[nums[k]] += 1
+                hmap[nums[j]] += 1
+            hmap[nums[i]] += 1
+        
+        res = []
+        for i in ans:
+            res += list(i),
+        return res
