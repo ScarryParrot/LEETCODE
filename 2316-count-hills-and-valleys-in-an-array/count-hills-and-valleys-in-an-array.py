7977bc7c126bdfecf2c0908eb79224bfd071e730
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2024-07-09 01:39:22 +0530

    Time: 42 ms (47.01%) | Memory: 16.6 MB (31.53%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
@@ -1,14 +1,18 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
-        c=0
-        cleaned_nums = [nums[0]]
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
+        c = 0
+        # Clean up the input by removing consecutive duplicates
+        cleaned_nums = []
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i - 1]:
                 cleaned_nums.append(nums[i])
+        
+        # Count hills and valleys in the cleaned list
         for i in range(1, len(cleaned_nums) - 1):
-            if cleaned_nums[i] > cleaned_nums[i - 1] and cleaned_nums[i] > cleaned_nums[i + 1]:
-                c += 1
-            elif cleaned_nums[i] < cleaned_nums[i - 1] and cleaned_nums[i] < cleaned_nums[i + 1]:
-                c += 1
+            if cleaned_nums[i] != cleaned_nums[i - 1] and cleaned_nums[i] != cleaned_nums[i + 1]:
+                if cleaned_nums[i] == max(cleaned_nums[i - 1], cleaned_nums[i], cleaned_nums[i + 1]):
+                    c += 1
+                elif cleaned_nums[i] == min(cleaned_nums[i - 1], cleaned_nums[i], cleaned_nums[i + 1]):
+                    c += 1
+        
         return c
-                
