778764cecc893bdd6ed18084b81c3786f71d64db
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2024-07-09 01:37:58 +0530

    Time: 46 ms (19.73%) | Memory: 16.6 MB (31.53%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
@@ -1,18 +1,14 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
-        c = 0
-        
-        # Clean up the input by removing consecutive duplicates
+        c=0
         cleaned_nums = [nums[0]]
         for i in range(1, len(nums)):
             if nums[i] != nums[i - 1]:
                 cleaned_nums.append(nums[i])
-        
-        # Count hills and valleys in the cleaned list
         for i in range(1, len(cleaned_nums) - 1):
             if cleaned_nums[i] > cleaned_nums[i - 1] and cleaned_nums[i] > cleaned_nums[i + 1]:
                 c += 1
             elif cleaned_nums[i] < cleaned_nums[i - 1] and cleaned_nums[i] < cleaned_nums[i + 1]:
                 c += 1
-                
         return c
+                
