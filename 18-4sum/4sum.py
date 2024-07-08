1d06af64841526e9dadce08af3ce9935b2de1c8f
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2024-07-08 13:16:43 +0530

    Time: 427 ms (72.39%) | Memory: 16.6 MB (27.30%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,35 +1,30 @@
-# Python3
-# Optimal Solution
+from typing import List
+
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n = len(nums)
         nums.sort()
-        res = []
-
-        for i in range(n-3):
-            # avoid the duplicates while moving i
+        results = []
+        length = len(nums)
+        
+        for i in range(length - 3):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            for j in range(i+1, n-2):
-                # avoid the duplicates while moving j
+            for j in range(i + 1, length - 2):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
-                lo = j + 1
-                hi = n - 1
-                while lo < hi:
-                    temp = nums[i] + nums[j] + nums[lo] + nums[hi]
-                    if temp == target:
-                        res += [nums[i], nums[j], nums[lo], nums[hi]],
-
-                        # skip duplicates
-                        while lo < hi and nums[lo] == nums[lo + 1]:
-                            lo += 1
-                        lo += 1
-                        while lo < hi and nums[hi] == nums[hi - 1]:
-                            hi -= 1
-                        hi -= 1
-                    elif temp < target:
-                        lo += 1
+                left, right = j + 1, length - 1
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+                    if total == target:
+                        results.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+                    elif total < target:
+                        left += 1
                     else:
-                        hi -= 1
-        return res
+                        right -= 1
+        return results
