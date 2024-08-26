afdd32a5e617b3d59112199cce0574de64020a9a
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2024-08-26 22:46:11 +0530

    Time: 126 ms (16.68%) | Memory: 19.3 MB (10.23%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         dict={}
-        for num in nums:
-            if num not in dict:
-                dict[num]=1
+        for i in range(len(nums)):
+            if nums[i] not in dict:
+                dict[nums[i]]=1
             else:
-                dict[num]+=1
-        for num,count in dict.items():
-            if count ==1:
-                return num
+                dict[nums[i]]+=1
+        print(dict)
+        for i,c in dict.items():
+            if c == 1:
+                return i
