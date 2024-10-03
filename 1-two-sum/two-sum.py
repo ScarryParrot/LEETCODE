773a20736e5129c0cbd32ee5327451ef57fa1b11
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2024-10-03 23:03:39 +0530

    Time: 63 ms (48.69%) | Memory: 18 MB (8.93%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict={}
-        for i,c in enumerate(nums):
-            diff=target-c
-            if c in dict:
-                return [dict[c],i]
-            else:
-                dict[diff]=i
-        return []
+        dict = {}  # Dictionary to store the numbers and their indices
+        
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            
+            # Check if the current number's complement (diff) is in the dictionary
+            if nums[i] in dict:
+                return [dict[nums[i]], i]  # Return the stored index and current index
+            
+            # Otherwise, store the current number's complement and its index in the dictionary
+            dict[diff] = i
