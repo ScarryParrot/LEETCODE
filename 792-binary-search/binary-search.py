09c18360e016b26177684dc1f934a58b65ad2fd4
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2024-10-08 15:48:36 +0530

    Time: 194 ms (38.81%) | Memory: 18.1 MB (73.98%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,6 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i,c in enumerate(nums):
+            if c == target:
+                return i
+        return -1
