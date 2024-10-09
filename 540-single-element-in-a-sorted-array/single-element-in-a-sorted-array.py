40f7b970443228b7bb930366ec13799608ccf81c
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2024-10-09 06:47:30 +0530

    Time: 122 ms (99.19%) | Memory: 26.7 MB (62.53%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -3,19 +3,12 @@ from typing import List
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         low, high = 0, len(nums) - 1
-        
         while low < high:
             mid = low + (high - low) // 2
-            
-            # Ensure `mid` is even for correct pair checking
             if mid % 2 == 1:
                 mid -= 1
-            
-            # If the pair is valid, move `low` to `mid + 2`
             if nums[mid] == nums[mid + 1]:
                 low = mid + 2
             else:
                 high = mid
-        
-        # `low` will point to the single element
         return nums[low]
