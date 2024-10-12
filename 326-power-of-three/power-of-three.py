864a173c89869a99f6e128cb9b2ad7033be5b774
ScarryParrot <110553838+ScarryParrot@users.noreply.github.com>
2024-10-12 11:38:53 +0530

    Time: 110 ms (6.59%) | Memory: 16.7 MB (15.37%) - LeetSync

---
+++ 326-power-of-three/power-of-three.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n < 0 or n == 0:
+            return False
+        def po(x):
+            # prod=1
+            prod=pow(3,x)
+            if prod == n:
+                return True
+            if prod > n:
+                return False
+            return po(x+1)
+        return po(0)
